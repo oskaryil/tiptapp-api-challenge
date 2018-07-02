ce814f7467e8cd5798d77fcad80616b6837d09d4
Oskar Yildiz <yildizoskar@gmail.com>
2018-07-02 23:45:26 +0200

    Added koa-logger for logging requests

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,14 +1,14 @@
+const logger = require('koa-logger');
+
 const db = require('./database');
 const router = require('../routes');
 
 module.exports = app => {
 
   // request logger
-  app
-    .use(async (ctx, next) => {
-      console.log(ctx.method, ctx.path, ctx.status);
-      await next();
-    });
+  app.use(logger());
+
+  app.context.db = db;
 
   app
     .use(router.routes())
