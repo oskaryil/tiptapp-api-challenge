fd8144acb91f10494ad04dcfa5dcb9e38a46d14d
Oskar Yildiz <yildizoskar@gmail.com>
2018-07-03 09:46:59 +0200

    Added JSDOC comment for createTodo and nice error message formatting

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -5,19 +5,31 @@ const eventTypes = {
   CREATE_TODO: 'create_todo'
 };
 
+/**
+ * @function createTodo
+ *
+ * @description
+ * DOING: Should create a Todo and an Event upon request
+ *
+ * @param  {Object}  ctx
+ * @returns
+ */
 const createTodo = async ctx => {
   try {
     const { title } = ctx.request.body;
+
     const newTodo = new Todo();
     const newEvent = new Event({ name: eventTypes.CREATE_TODO });
+
     newTodo.title = title;
     await newTodo.save();
     await newEvent.save();
+
     ctx.status = 201;
     ctx.body = { message: 'Todo created', todo: newTodo };
   } catch (err) {
     ctx.status = 400;
-    ctx.body = err.message;
+    ctx.body = { message: 'An error occured', error: err.message };
   }
 };
 
