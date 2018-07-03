3d776fd07999e59633770af087becff3bff1f674
Oskar Yildiz <yildizoskar@gmail.com>
2018-07-03 09:44:15 +0200

    Added event types and saving

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -1,11 +1,18 @@
 const Todo = require('../models/todo.model');
+const Event = require('../models/event.model');
+
+const eventTypes = {
+  CREATE_TODO: 'create_todo'
+};
 
 const createTodo = async ctx => {
   try {
     const { title } = ctx.request.body;
     const newTodo = new Todo();
+    const newEvent = new Event({ name: eventTypes.CREATE_TODO });
     newTodo.title = title;
     await newTodo.save();
+    await newEvent.save();
     ctx.status = 201;
     ctx.body = { message: 'Todo created', todo: newTodo };
   } catch (err) {
