789e320c089bf52dc70060b2b60c636ba11e6ba5
Oskar Yildiz <yildizoskar@gmail.com>
2018-07-03 11:38:05 +0200

    Added function and route for fetching all todos

--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -5,6 +5,7 @@ const TodoController = require('../controllers/todo.controller');
 const router = new Router();
 
 router
+  .get('/', TodoController.fetchTodos)
   .post('/', TodoController.createTodo)
   .delete('/:id', TodoController.deleteTodo)
   .patch('/:id', TodoController.updateTodo);
